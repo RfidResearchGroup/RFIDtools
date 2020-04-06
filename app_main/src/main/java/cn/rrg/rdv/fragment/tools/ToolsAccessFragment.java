01f4f0eed09c0f4a9cab747fde38fe0209dcd2b1
dxl <64101226@qq.com>
2020-04-06 23:44:49 +0800

    Removed module "mfkey" to maven(gitpack.io)

--- app_main/src/main/java/cn/rrg/rdv/fragment/tools/ToolsAccessFragment.java
+++ app_main/src/main/java/cn/rrg/rdv/fragment/tools/ToolsAccessFragment.java
@@ -13,15 +13,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import cn.rrg.rdv.R;
-import cn.rrg.rdv.activities.tools.DumpEditActivity;
 import cn.rrg.rdv.activities.tools.DumpEqualActivity;
 import cn.rrg.rdv.activities.tools.DumpListActivity;
 import cn.rrg.rdv.activities.tools.FileListActivity;
 import cn.rrg.rdv.activities.tools.FormatCovertActivity;
-import cn.rrg.rdv.activities.tools.KeyFileEditActivity;
 import cn.rrg.rdv.activities.tools.KeyFileListActivity;
-import cn.rrg.rdv.activities.tools.MfKey32ConsoleActivity;
-import cn.rrg.rdv.activities.tools.MfKey64ConsoleActivity;
 import cn.rrg.rdv.fragment.base.BaseFragment;
 
 public class ToolsAccessFragment
@@ -29,11 +25,8 @@ public class ToolsAccessFragment
 
     private Button btnDataEditor = null;
     private Button btnKeyEditor = null;
-    private Button btnUnionKeyEditor = null;
     private Button btnFormatCovert = null;
     private Button btnDiffTools = null;
-    private Button btnMfKey32 = null;
-    private Button btnMfKey64 = null;
 
     @Nullable
     @Override
@@ -52,20 +45,11 @@ public class ToolsAccessFragment
     private void initViews(View view) {
         btnDataEditor = view.findViewById(R.id.btnDumpEditor);
         btnKeyEditor = view.findViewById(R.id.btnKeyEditor);
-        btnUnionKeyEditor = view.findViewById(R.id.btnUnionKeyEditor);
         btnFormatCovert = view.findViewById(R.id.btnConvertFormat);
         btnDiffTools = view.findViewById(R.id.btnDiffTool);
-        btnMfKey32 = view.findViewById(R.id.btnMfKey32);
-        btnMfKey64 = view.findViewById(R.id.btnMfKey64);
     }
 
     private void initActions(Context context) {
-        //mfkey32工具的跳转!
-        btnMfKey32.setOnClickListener(v -> startActivity(new Intent(context, MfKey32ConsoleActivity.class)));
-
-        //mfkey64工具的跳转!
-        btnMfKey64.setOnClickListener(v -> startActivity(new Intent(context, MfKey64ConsoleActivity.class)));
-
         //设置数据编辑的按钮的接口实现
         btnDataEditor.setOnClickListener(v -> {
             startActivity(new Intent(context, DumpListActivity.class).putExtra("mode", DumpListActivity.MODE.EDIT));
