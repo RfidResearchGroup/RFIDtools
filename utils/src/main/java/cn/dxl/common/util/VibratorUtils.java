45587f276237ce35dbcc254e266b4482ee28c92e
dxl <64101226@qq.com>
2020-06-29 18:14:04 +0800

    Fixed Vibrator maybe null.

--- utils/src/main/java/cn/dxl/common/util/VibratorUtils.java
+++ utils/src/main/java/cn/dxl/common/util/VibratorUtils.java
@@ -14,7 +14,7 @@ public class VibratorUtils {
     //进行抖动!
     public static void runOneAsDelay(Context context, int delay) {
         Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        if (vibrator.hasVibrator()) { //有振动器
+        if (vibrator != null && vibrator.hasVibrator()) { //有振动器
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
                 VibrationEffect vibrationEffect = VibrationEffect.createOneShot(delay, VibrationEffect.DEFAULT_AMPLITUDE);
                 vibrator.vibrate(vibrationEffect);
