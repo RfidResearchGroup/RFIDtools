60ab62b7db62db409176969806140faf0d7d9c59
dxl <64101226@qq.com>
2020-07-03 16:35:32 +0800

    moved

--- app_main/src/main/java/cn/rrg/rdv/activities/proxmark3/rdv4_rrg/Proxmark3NewTerminalInitActivity.java
+++ app_main/src/main/java/cn/rrg/rdv/activities/proxmark3/rdv4_rrg/Proxmark3NewTerminalInitActivity.java
@@ -1,15 +1,17 @@
-package cn.rrg.rdv.activities.tools;
+package cn.rrg.rdv.activities.proxmark3.rdv4_rrg;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.Nullable;
 
 import com.termux.app.TermuxActivity;
 
+import cn.dxl.common.widget.ToastUtil;
 import cn.rrg.rdv.R;
 import cn.rrg.rdv.activities.main.BaseActivity;
 import cn.rrg.rdv.activities.main.PM3FlasherMainActivity;
@@ -17,11 +19,29 @@ import cn.rrg.rdv.util.Commons;
 import cn.rrg.rdv.util.Proxmark3Installer;
 
 public class Proxmark3NewTerminalInitActivity extends BaseActivity {
+
+    private RadioGroup rdoGroupTerminalSelect;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_pm3_terminal_init);
 
+        rdoGroupTerminalSelect = findViewById(R.id.rdoGroupTerminalSelect);
+        rdoGroupTerminalSelect.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rdoBtnFullTerminal:
+                        Commons.setTerminalType(0);
+                        break;
+                    case R.id.rdoBtnSimpleTerminal:
+                        Commons.setTerminalType(1);
+                        break;
+                }
+            }
+        });
+        rdoGroupTerminalSelect.check(checkTerminalType());
 
         findViewById(R.id.btnFwFlash).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +53,10 @@ public class Proxmark3NewTerminalInitActivity extends BaseActivity {
         findViewById(R.id.btnGoToTermux).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (rdoGroupTerminalSelect.getCheckedRadioButtonId() == -1) {
+                    ToastUtil.show(context, getString(R.string.tips_terminal_select_like), false);
+                    return;
+                }
                 if (Commons.isPM3ClientDecompressed()) {
                     go();
                 } else {
@@ -50,18 +74,36 @@ public class Proxmark3NewTerminalInitActivity extends BaseActivity {
         box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Commons.setAutoGoToTermux(isChecked);
+                Commons.setAutoGoToTerminal(isChecked);
             }
         });
 
         // Must have init pm3 client and check auto go!
-        if (Commons.getAutoGoToTermux() && Commons.isPM3ClientDecompressed()) {
+        if (Commons.getAutoGoToTerminal() && Commons.isPM3ClientDecompressed()) {
             go();
         }
     }
 
     public void go() {
-        startActivity(new Intent(this, TermuxActivity.class));
+        if (Commons.getTerminalType() == 0) {
+            startActivity(new Intent(this, TermuxActivity.class));
+        } else {
+            startActivity(new Intent(this, Proxmark3Rdv4RRGRedTeamConsoleActivity.class));
+        }
         finish();
+    }
+
+    private int checkTerminalType() {
+        switch (Commons.getTerminalType()) {
+            case -1:
+            default:
+                return -1;
+
+            case 0:
+                return R.id.rdoBtnFullTerminal;
+
+            case 1:
+                return R.id.rdoBtnSimpleTerminal;
+        }
     }
 }
